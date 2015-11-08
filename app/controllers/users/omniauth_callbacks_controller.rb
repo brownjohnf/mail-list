dd062848df9e517879478c1d2c51d997d4a61207
John (Jack) Brown <jack@brownjohnf.com>
2015-11-07 17:17:58 -0800

    Fix github oauth for localhost

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,19 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     # You need to implement the method below in your model (e.g. app/models/user.rb)
+    puts 'start'
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    puts 'found user'
 
-    puts @user.inspect
-    puts request.env["omniauth.auth"].inspect
+    puts 'signing in'
+    puts sign_in(@user).inspect
+    puts 'signed in'
 
     if @user.persisted?
       #this will throw if @user is not activated
       sign_in_and_redirect @user, :event => :authentication
 
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
     else
       # session["devise.github_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
+      user.confirmed_at = Time.now
+      user.confirmation_token = nil
+      user.confirmation_sent_at = nil
     end
   end
 end
