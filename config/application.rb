9bcfdeb2ad7c2c81eba6128697abf2237a0c652b
John (Jack) Brown <jack@brownjohnf.com>
2015-11-08 15:00:05 -0800

    Configure puma

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module NwAuth
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.action_mailer.default_url_options = { host: ENV['APP_HOST'], port: ENV['APP_PORT'] }
+    config.action_mailer.default_url_options = { host: ENV['APP_HOST'], port: ENV['PORT'] }
   end
 end
